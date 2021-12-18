4285a023f88fd51370380ac9c3fac6fd429b73e3
ivalkshfoeif <61750044+ivalkshfoeif@users.noreply.github.com>
2021-12-17 23:35:44 -0500

    main

---
+++ Diferenca.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int A, B, C, D;
+    cin >> A >> B >> C >> D;
+    cout << "DIFERENCA = ";
+    cout << A * B - C * D << endl;
+}
