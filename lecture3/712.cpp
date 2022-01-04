80d27dbf7cbcfa815ee0bce1e3263951ad50f5c9
ivalkshfoeif <61750044+ivalkshfoeif@users.noreply.github.com>
2022-01-04 00:58:41 -0500

    solution

--- lecture3/712.cpp
+++ lecture3/712.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 
 int main(){
+    int n = 6, count = 0;
+
+    while(n--){
+        double c;
+        cin >> c;
+        if (c > 0)
+            count++;
+    }
+
+    printf("%d positive numbers", count);
 
     return 0;
 }
