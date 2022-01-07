7378f3ff998285438f5ff8eb0326eac850c09493
ivalkshfoeif <61750044+ivalkshfoeif@users.noreply.github.com>
2022-01-07 03:58:38 -0500

    solution

--- lecture5/768.cpp
+++ lecture5/768.cpp
@@ -10,8 +10,31 @@ int main(){
     int len1 = s1.size(), len2 = s2.size(), len = min(len1, len2);
 
     for (int i = 0; i < len; i++){
+        if (s1[i] >= 'A' && s1[i] <= 'Z'){
+            s1[i] += 32;
+        }
+        if (s2[i] >= 'A' && s2[i] <= 'Z'){
+            s2[i] += 32;
+        }
 
-        
+
+        if (s1[i] < s2[i]){
+            puts("<");
+            return 0;
+        }if (s1[i] > s2[i]){
+            puts(">");
+            return 0;
+        }
+    }
+
+    if (len1 == len2){
+        puts("=");
+    }else {
+        if (len1 > len2){
+            puts(">");
+        }else {
+            puts("<");
+        }
     }
 
         return 0;
