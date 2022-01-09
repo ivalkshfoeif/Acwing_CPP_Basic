c5ca1480a6a04d9b48dc5a19909035b9769f56f1
ivalkshfoeif <61750044+ivalkshfoeif@users.noreply.github.com>
2022-01-08 22:43:53 -0500

    solution

--- lecture5/764.cpp
+++ lecture5/764.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 
 int main(){
+    string a;
+    getline(cin, a);
+    int len = a.size();
+
+    for (int i = 0; i < len; i++){
+        cout << char (a[i] + a[(i + 1) % len]);
+    }
 
     return 0;
 }
