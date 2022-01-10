8beb2fdfda38617182616bdfb1b5f010f0890706
ivalkshfoeif <61750044+ivalkshfoeif@users.noreply.github.com>
2022-01-10 01:38:34 -0500

    solution

--- lecture5/778.cpp
+++ lecture5/778.cpp
@@ -1,9 +1,34 @@
-#include<bits/stdc++.h>
+#include<iostream>
 
 using namespace std;
 
 int main(){
-    
+    string s, s1, s2;
+    getline(cin, s, ',');
+    getline(cin, s1, ',');
+    getline(cin, s2, '\n');
+    int slen = s.size();
+    int s1len = s1.size();
+    int s2len = s2.size();
+    int p1 = -1, p2 = -1;
+    for (int i = 0; i < slen - s1len; i++){
+        if (s.substr(i, s1len) == s1){
+            p1 = i;
+            break;
+        }
+    }
+
+    for (int i = 0; i < slen - s2len + 1; i++){
+        if (s.substr(i, s2len) == s2){
+            p2 = i;
+        }
+    }
+    if (p2 - p1 < s1len || p1 == -1 || p2 == -1){
+        cout << -1 << endl;
+        return 0;
+    }
+
+    cout << p2 - p1 - s1len << endl;
 
     return 0;
 }
---
+++ lecture5/779.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int N = 200;
+
+int n;
+string str[N];
+
+int main(){
+    while(cin >> n && n != 0){
+        int len = 1000;
+        for (int i = 0; i < n; i++){
+            cin >> str[i];
+            if (len > str[i].size()){
+                len = str[i].size();
+            }
+        }
+
+        while(len >= 0){
+            bool success = true;
+            for (int i = 1; i < n; i++){
+                bool is_same = true;
+                for (int j = 1; j <= len; j++){
+                    if (str[0][str[0].size() - j] != str[i][str[i].size() - j]){
+                        is_same = false;
+                        break;
+                    }
+                }
+                if (!is_same){
+                    success = false;
+                    break;
+                }
+            }
+
+            if (!success){
+                len--;
+            }else {
+                break;
+            }
+        }
+
+        cout <<  str[0].substr(str[0].size() - len) << endl;
+    }
+
+    return 0;
+}
